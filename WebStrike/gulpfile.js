c8d1b6e0154e5bfcfee83cedad1d2e80382326d8
vasileios.bomponis@nebest.nl <vasileios.bomponis@nebest.nl>
2022-09-17 13:57:51 +0200

    add ts,sass compiler

---
+++ WebStrike/gulpfile.js
@@ -0,0 +1,44 @@
+/// <binding AfterBuild='default' />
+let gulp = require('gulp')
+let gulp_sass = require('gulp-sass')(require('sass'))
+let gulp_ts = require('gulp-typescript')
+
+let filePaths = {
+    sassInputPath: "./Sass/**/*.scss",
+    sassOutputPath: "./wwwroot/css",
+    tsInputPath: "./scripts/**/*.ts",
+    tsOutputPath: "./wwwroot/js",
+}
+
+gulp.task('build-sass', () => {
+    return gulp.src(filePaths.sassInputPath)
+        .pipe(gulp_sass())
+        .pipe(gulp.dest(filePaths.sassOutputPath))
+})
+
+gulp.task('build-ts', () => {
+    return gulp.src(filePaths.tsInputPath)
+        .pipe(gulp_ts())
+        .pipe(gulp.dest(filePaths.tsOutputPath))
+})
+
+gulp.task('watch', () => {
+    gulp.watch(filePaths.sassInputPath, gulp.series('build-sass'))
+    gulp.watch(filePaths.tsInputPath, gulp.series('build-ts'))
+})
+
+exports.default = gulp.parallel('build-ts', 'build-sass', 'watch')
+
+//var gulp = require("gulp");
+//var del = require("del");
+//var paths = {
+//    scripts: ["scripts/**/*.js", "scripts/**/*.ts", "scripts/**/*.map"],
+//};
+//gulp.task("clean", function () {
+//    return del(["wwwroot/scripts/**/*"]);
+//});
+//gulp.task("default", function (done) {
+//    gulp.src(paths.scripts).pipe(gulp.dest("wwwroot/scripts"));
+//    done();
+//});
+
